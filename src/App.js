4661159524bb05b1fbbb549b0092e5533ce4ab16
Spectre114 <79752106+Spectre114@users.noreply.github.com>
2023-09-19 11:49:27 +0530

    Final_Draft3

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import HeroSection from "./components/HeroSection";
 import Navbar from "./components/Navbar";
 import ContentSection from "./Pages/ContentSection";
+import Footer from "./Pages/Footer";
 import PricingSection from "./Pages/PricingSection";
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <HeroSection></HeroSection>
       <ContentSection></ContentSection>
       <PricingSection></PricingSection>
+      <Footer></Footer>
     </div>
   );
 }
